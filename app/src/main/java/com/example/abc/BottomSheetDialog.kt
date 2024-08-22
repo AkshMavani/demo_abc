185b091be6f6c46ceb8a9cf97245d034f0978cc5
AkshMavani <mavaniaksh3824@gmail.com>
2024-08-22 22:21:45 +0530

    data1234

--- app/src/main/java/com/example/abc/BottomSheetDialog.kt
+++ app/src/main/java/com/example/abc/BottomSheetDialog.kt
@@ -5,46 +5,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.blur.Adapter_StickerView
 import com.example.demo_full.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
+
 class BottomSheetDialog : BottomSheetDialogFragment() {
     @SuppressLint("MissingInflatedId")
+    var arr:ArrayList<String> = ArrayList()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.bottom_sheet_layout, container, false)
-        val inbox = view.findViewById<TextView>(R.id.TvInbox)
-        val document = view.findViewById<TextView>(R.id.TvDocumet)
-        val camera = view.findViewById<TextView>(R.id.TvCamera0)
-        val gallery = view.findViewById<TextView>(R.id.TvGallery0)
-        val googleplus = view.findViewById<TextView>(R.id.TvGoogleplus)
+        val recyclerView:RecyclerView = view.findViewById<RecyclerView>(R.id.Tvopen)
+        val layoutManager = GridLayoutManager(context, 4)
 
-        inbox.setOnClickListener {
-            Toast.makeText(activity, " Inbox", Toast.LENGTH_SHORT).show()
-            dismiss()
-        }
-        document.setOnClickListener {
-            Toast.makeText(activity, " Document", Toast.LENGTH_SHORT).show()
-            dismiss()
-        }
-        camera.setOnClickListener {
-            Toast.makeText(activity, " camera", Toast.LENGTH_SHORT).show()
-            dismiss()
-        }
-        gallery.setOnClickListener {
-            Toast.makeText(activity, " gallery", Toast.LENGTH_SHORT).show()
-            dismiss()
-        }
-        googleplus.setOnClickListener {
-            Toast.makeText(activity, " googleplus", Toast.LENGTH_SHORT).show()
-            dismiss()
-        }
+        recyclerView.layoutManager = layoutManager
+        arr.add(str)
+        arr.add(str1)
+        arr.add(str2)
+        arr.add(str3)
+        arr.add(str4)
+        arr.add(str5)
+        arr.add(str6)
+        arr.add(str7)
+        val adapter= context?.let { Adapter_StickerView(arr, it) }
+        recyclerView.adapter=adapter
+
         return view
     }
 
     override fun getTheme(): Int {
         return R.style.AppBottomSheetDialogTheme
     }
-
+   companion object{
+       var str="/storage/emulated/0/Pictures/IMG_20240818_191840_1.jpg"
+       var str1="/storage/emulated/0/Pictures/IMG_20240818_191840.jpg"
+       var str2="/storage/emulated/0/Pictures/IMG_20240818_191840.jpg"
+       var str3="/storage/emulated/0/Pictures/IMG_20240818_191839.jpg"
+       var str4="/storage/emulated/0/Pictures/IMG_20240818_191840_1.jpg"
+       var str5="/storage/emulated/0/Pictures/IMG_20240818_191840.jpg"
+       var str6="/storage/emulated/0/Pictures/IMG_20240818_191840.jpg"
+       var str7="/storage/emulated/0/Pictures/IMG_20240818_191839.jpg"
+   }
 }
---
+++ app/src/main/java/com/example/blur/Adapter_StickerView.kt
@@ -0,0 +1,50 @@
+package com.example.blur
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.demo_full.Model_Img
+import com.example.demo_full.R
+
+class Adapter_StickerView(private val mList: ArrayList<String>, var context: Context) : RecyclerView.Adapter<Adapter_StickerView.ViewHolder>() {
+
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        // inflates the card_view_design view
+        // that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_view_design, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val ItemsViewModel = mList[position]
+        Log.e("ITEM", "onBindViewHolder: >>$ItemsViewModel", )
+        Glide.with(context)
+            .load(ItemsViewModel)
+            .into(holder.imageView) // imageView is your ImageView where you want to display the blurred
+
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.img_blur_layout)
+
+    }
+
+
+}
